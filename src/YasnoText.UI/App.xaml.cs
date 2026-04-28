084aad025dae8611651613a5b2dff4388e060384
Serouke <haconar@mail.ru>
2026-04-28 11:58:31 +0300

    feat: add theme system with three resource dictionaries

--- src/YasnoText.UI/App.xaml.cs
+++ src/YasnoText.UI/App.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using YasnoText.UI.Themes;
 
 namespace YasnoText.UI;
 
@@ -9,5 +10,12 @@ namespace YasnoText.UI;
 /// </summary>
 public partial class App : Application
 {
-}
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
 
+        // При старте приложения применяем стандартную тему,
+        // чтобы все ресурсы (цвета, шрифты, размеры) были доступны.
+        ThemeManager.ApplyTheme("Standard");
+    }
+}
